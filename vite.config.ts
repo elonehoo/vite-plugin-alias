a9329385f78f6f2ce4969b2b66b7390af8b960a3
elonehoo <huchengyea@163.com>
2022-10-31 13:55:45 +0800

    init:first commit

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types="vitest" />
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import types from '@elonehoo/vite-plugin-type-ts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    types({
+      tsConfigFilePath: './tsconfig.json',
+      cleanVueFileName: true,
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
